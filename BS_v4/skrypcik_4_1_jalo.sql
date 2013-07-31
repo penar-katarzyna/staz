87ff936c40ae4f4dc5938f96f2265b459aa7463c
Paweł Jałocha <jaloszek@gmail.com>
2013-07-31 16:08:22 +0200

    jalo2

--- BS_v4/skrypcik_4_1_jalo.sql
+++ BS_v4/skrypcik_4_1_jalo.sql
@@ -4,7 +4,8 @@
 
 @C:\Users\Comarch\Documents\GitHub\staz\BS_v4\biladmin_4_0.sql
 @C:\Users\Comarch\Documents\GitHub\staz\BS_v4\TABELE\tabele_4_1.sql
-pawel
+--pawel
 @C:\Users\Comarch\Documents\GitHub\staz\BS_v4\FUNKCJE\funkcje_4_1.sql
 @C:\Users\Comarch\Documents\GitHub\staz\BS_v4\DANE\dane_4_1.sql
 @C:\Users\Comarch\Documents\GitHub\staz\BS_v4\prawa_do_schematu_4_0.sql
+--JAREK !  gdzieś ty ?
